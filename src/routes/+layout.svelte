81d989daea6554cf677407776c1bf302ccac30d3
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-05-06 15:40:24 +0800

    feat: add component to handle authentication from the app header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 	// Auth event listener for Supabase
 	import { goto, invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
+	import AppHeaderAuthComponent from '$lib/components/AppHeaderAuthComponent.svelte';
 
 	export let data;
 	$: ({ session, supabase } = data);
@@ -47,30 +48,7 @@
 				<strong class="text-xl uppercase">Skeleton</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<AppHeaderAuthComponent supabase={supabase}/>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
