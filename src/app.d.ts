24dac1bf826278c829ce977345281e1b8beb5c1f
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-05-21 03:19:44 +0800

    feat: initialize and trash mqtt client at appropriate points

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,10 +1,12 @@
 import type { Session, SupabaseClient, User } from '@supabase/supabase-js'
+import type { MqttClient } from 'mqtt';
 
 declare global {
   namespace App {
     // interface Error {}
     interface Locals {
       supabase: SupabaseClient
+      mqttClient: MqttClient
       safeGetSession: () => Promise<{ session: Session | null; user: User | null }>
       session: Session | null
       user: User | null
