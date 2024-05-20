b841497a7eba246e5eae31c33d67fb8665da0705
ajogerio <aaogerio@up.edu.ph>
2024-05-20 19:08:39 +0800

    Feat: added new toggle lock page

---
+++ src/routes/auth/toggle_lock/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import smiley from './images/smileyface.jpg';
+	import concerned from './images/gif.gif';
+
+	let isLocked = true;
+
+	const toggleIsLocked = () => {
+		isLocked = !isLocked;
+	};
+</script>
+
+{#if isLocked}
+	<img alt="Smiley face" src={smiley} />
+{:else}
+	<img alt="Concerned face" src={concerned} />
+{/if}
+
+<div class="buttonContainer" on:click={toggleIsLocked}>
+	<button>
+		{#if isLocked}
+			Unlock
+		{:else}
+			Lock
+		{/if}
+	</button>
+</div>
+
+<style>
+	.buttonContainer {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+</style>
