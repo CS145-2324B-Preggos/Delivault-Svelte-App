370183c0792028ffc7469374d76ab4d95939d7b9
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-05-21 19:05:42 +0800

    remove user functions and types

--- src/lib/server/BoxSB.ts
+++ src/lib/server/BoxSB.ts
@@ -8,17 +8,24 @@ const success: BoxResponse = {
 	error: null
 };
 
-export async function selectBoxDB(filter: BoxFilter, supabase: SupabaseClient): Promise<BoxResponse> {
-    let query = supabase
-    .from('box')
-    .select('box_id, user_id');
+export async function selectBoxDB(filter: BoxFilter | null, supabase: SupabaseClient): Promise<BoxResponse> {
+    let query;
+    if (filter == null) {
+        let query = supabase
+        .from('box')
+        .select();
+    } else{
+        let query = supabase
+        .from('box')
+        .select('box_id, user_id');
 
-    if (filter.box_id != null){
-        query = query.eq('box_id',filter.box_id);
-    }
+        if (filter.box_id != null){
+            query = query.eq('box_id',filter.box_id);
+        }
 
-    if (filter.user_id != null) {
-        query = query.eq('user_id',filter.user_id);
+        if (filter.user_id != null) {
+            query = query.eq('user_id',filter.user_id);
+        }
     }
 
     // execute query
