693b66ec2810c9d93f7d5d0058a7b322f32cd696
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-05-22 17:01:28 +0800

    chore: remove login api endpoint

--- src/routes/api/box/+server.ts
+++ src/routes/api/box/+server.ts
@@ -3,20 +3,20 @@ import { json } from "@sveltejs/kit";
 
 // Requests for viewing all boxes (perhaps of a certain user, can be tweaked in the filters)
 
-export async function POST({ request }, locals: { supabase } ) {
+export async function POST({ request, locals } ) {
 	/* Handles Select requests for admin records. */
 	const filter = await request.json();
-	return json(await Box.selectBoxes(filter, supabase));
+	return json(await Box.selectBoxes(filter, locals.supabase));
 }
 
-export async function PATCH({ request }, locals: { supabase }) {
+export async function PATCH({ request, locals }) {
 	/* Handles Update and Approve requests for admin records. */
 	const updateInfo = Box.toBoxObject(await request.json());
-	return json(await Box.updateBox(updateInfo, supabase));
+	return json(await Box.updateBox(updateInfo, locals.supabase));
 }
 
-export async function DELETE({ request }, locals: { supabase }) {
+export async function DELETE({ request, locals }) {
 	/* Handles Delete requests for admin records. */
 	const box = await request.json();
-	return json(await Box.deleteBox(box.box_id, supabase));
+	return json(await Box.deleteBox(box.box_id, locals.supabase));
 }
