cb394f6f0f568d84ef8fe385ac8f0fd6ce6c9fe8
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-05-27 15:13:16 +0800

    feat: enforce https

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
+import fs from 'fs';
 
 export default defineConfig({
 	plugins: [
@@ -11,5 +12,12 @@ export default defineConfig({
 			compiler: 'svelte',
 			autoInstall: true
 		})
-	]
+	],
+	server: {
+		https: {
+			key: fs.readFileSync(`${__dirname}/cert/key.pem`),
+			cert: fs.readFileSync(`${__dirname}/cert/cert.pem`) 
+		},
+		proxy: {},
+	}
 });
