d764e3b32180b15328f65e54e2645524cdaa9361
gabrielcalubayan72 <gscalubayan@up.edu.ph>
2024-05-29 01:42:21 +0800

    fix: bring up to working state by adjusting hash length, etc

--- src/routes/api/verify-passcode/+server.ts
+++ src/routes/api/verify-passcode/+server.ts
@@ -4,30 +4,37 @@ import { error, json } from '@sveltejs/kit'
 // check an 8-character passcode to see if it is valid
 export async function POST({ request, locals: { supabase, mqttClient } }) {
 
-    const requestObject: { hash_passcode: string, box_id: string } = await request.json()
+    // console.log(request)
+    const requestArray: { hash_passcode: string, box_id: string }[] = await request.json()
+    const requestObject = requestArray[0]
     
+    // preprocess requestObject.hash_passcode, truncate to first 16 bytes
+    requestObject.hash_passcode = requestObject.hash_passcode.substring(0, 16)
+    
+    // console.log(requestObject)
     const {data, error: pgError} = await supabase
-        .from("orders")
+        .from("public_orders")
         .select()
         .eq(
             'hash_passcode', requestObject.hash_passcode
-        ).eq(
-            'box_id', requestObject.box_id
         )
 
     if (pgError) error(400, `${pgError.code}: ${pgError.details}`)
     if (data.length == 0) error(400, 'invalid code')
 
-    const boxResponse = await sendControlMessage(mqttClient, requestObject.box_id, "unlock")
-
-    if (boxResponse.success) {
-        const {error: pgError} = await supabase
-            .from("orders")
-            .update({status: true})
-            .select()
-        
-        if (pgError) error(500, 'db update failed')
-    }
+    const boxResponse = sendControlMessage(mqttClient, requestObject.box_id, "unlock").then(
+        (boxResponse) => {
+            if (boxResponse.success) {
+                const {error: pgError} = supabase
+                    .from("public_orders")
+                    .update({status: true})
+                    .select()
+            
+            if (pgError) {console.log("db update error"); error(500, 'db update failed')}
+            console.log("Box response successful")
+        }}
+    )
+    
 
     return json(`valid code`)
 }
