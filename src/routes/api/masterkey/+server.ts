174878c1672e07352e73d09f1c674ac9f0957bc3
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-05-30 13:57:09 +0800

    fix: use locally initialized mqtt client

--- src/routes/api/masterkey/+server.ts
+++ src/routes/api/masterkey/+server.ts
@@ -1,16 +1,17 @@
-import { sendMasterkey } from "$lib/server/MQTT.js";
+import { initializeMQTTClient, sendMasterkey } from "$lib/server/MQTT.js";
 import { json } from "@sveltejs/kit";
 
 // publishing masterkey via supabase
 
-export async function PATCH({ request, locals } ) {
+export async function PATCH({ request } ) {
 	// this now should send a signal to the broker to lock and unlock, and should update the box in supabase
+    const mqttClient = await initializeMQTTClient();
     try {
         const passcodeResponse = await request.json();
         const boxMasterkey = passcodeResponse[0];
 
         // Send control message via MQTT
-        const sendMasterkeyResponse = await sendMasterkey(locals.mqttClient, boxMasterkey.box_id, boxMasterkey.masterkey);
+        const sendMasterkeyResponse = await sendMasterkey(mqttClient, boxMasterkey.box_id, boxMasterkey.masterkey);
 
         if (!sendMasterkeyResponse.success) {
             throw new Error(sendMasterkeyResponse.error);
