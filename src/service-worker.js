ff32417041856c70004daf5b2e283832af217969
Victor Edwin Reyes <victorereyesgg@gmail.com>
2024-05-31 18:13:22 +0800

    feat: add open page action to push notification push

--- src/service-worker.js
+++ src/service-worker.js
@@ -104,3 +104,24 @@ self.addEventListener(
 		)
 	}
 )
+
+// this code taken from an MDN web docs example
+self.addEventListener("notificationclick", (event) => {
+	console.log("On notification click: ", event.notification.tag);
+	event.notification.close();
+  
+	// This looks to see if the current is already open and
+	// focuses if it is
+	event.waitUntil(
+	  clients
+		.matchAll({
+		  type: "window",
+		})
+		.then((clientList) => {
+		  for (const client of clientList) {
+			if (client.url === "/" && "focus" in client) return client.focus();
+		  }
+		  if (clients.openWindow) return clients.openWindow("/");
+		}),
+	);
+  });
